65ebbffd007dd9be3a360f47155c6fab3b331e5f
Prism <230191336+prism3344@users.noreply.github.com>
2026-03-15 16:11:56 +0100

    Implement main entry point for Flask application
    Add main entry point to run the Flask app on a specified port.

--- vpn.py
+++ vpn.py
@@ -1,1 +1,4 @@
 from flask import Flask, request, Response import requests app = Flask(__name__) @app.route('/proxy/<path:url>') def proxy(url): target_url = f"https://{url}" if not url.startswith('http') else url head = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' } resp = requests.get(target_url, stream=True, headers=head) excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection'] headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers] return Response(resp.content, resp.status_code, headers) #teoretycznie tego statuscode i headers mogłoby nie być ale lepiej jak jest
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
