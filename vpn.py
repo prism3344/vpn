82c2be3af5ab419b74bd8a29bad553cfedebc0ad
Prism <230191336+prism3344@users.noreply.github.com>
2026-03-15 14:45:18 +0100

    Implement a simple proxy server using Flask

---
+++ vpn.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+@app.route('/proxy/<path:url>')
+def proxy(url):
+    target_url = f"https://{url}" if not url.startswith('http') else url
+    
+    resp = requests.get(target_url, stream=True)
+    
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    return Response(resp.content, resp.status_code, headers) #teoretycznie tego statuscode i headers mogłoby nie być ale lepiej jak jest
+
+if __name__ == '__main__':
+    app.run(port=5000)
