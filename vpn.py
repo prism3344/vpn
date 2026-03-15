1a7dc213b693921eee969d02fc14b5eb0117c60f
Prism <230191336+prism3344@users.noreply.github.com>
2026-03-15 15:55:46 +0100

    Add main block to run app with environment port

--- vpn.py
+++ vpn.py
@@ -30,3 +30,6 @@ def proxy(url):
 
     return Response(html, mimetype="text/html")
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
